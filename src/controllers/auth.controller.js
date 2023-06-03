07230d260f778cc7ff0c9db4d2644da878779f1b
Vndark5464 <nguyenhuulinh5464@gmail.com>
2023-06-03 11:12:15 +0700

    vvvv

--- src/controllers/auth.controller.js
+++ src/controllers/auth.controller.js
@@ -1,18 +1,31 @@
 const { use } = require("../routes/auth.route");
 const User = require("./../models/user");
 const bcrypt = require("bcryptjs");
-exports.register = (req,res)=>{
+const nodemailer = require("nodemailer");
+const config_mail = {
+    service: "Gmail",
+    host: "smtp.gmail.com",
+    port: 587,
+    auth: {
+        user: "nguyenhuulinh5464@gmail.com",
+        pass: "xouhfdcnnacnvgql"
+    }
+}
+const transport = nodemailer.createTransport(config_mail);
+
+exports.register = (req, res) => {
     const auth = req.session.auth;
-    if(auth) return res.send(`Bạn đã login bằng email ${auth.email}`);
+    if (auth) return res.send(`Bạn đã login bằng email ${auth.email}`);
     res.render("auth/register");
 }
-exports.create_user = async (req,res)=>{
+
+exports.create_user = async (req, res) => {
     try {
-        let existuser = await User.findOne({email:req.body.email});
-        if(existuser) return res.status(422).send("Email is exist..");
+        let existuser = await User.findOne({ email: req.body.email });
+        if (existuser) return res.status(422).send("Email is exist..");
         // hash password
         const salt = await bcrypt.genSalt(10);
-        const hashPwd = await bcrypt.hash(req.body.password,salt);
+        const hashPwd = await bcrypt.hash(req.body.password, salt);
         // save to db
         const user = new User({
             name: req.body.name,
@@ -22,7 +35,7 @@ exports.create_user = async (req,res)=>{
         await user.save();
         req.session.auth = {
             name: user.name,
-            email:user.email,
+            email: user.email,
         }
         res.send("DONE");
     } catch (error) {
@@ -30,15 +43,16 @@ exports.create_user = async (req,res)=>{
     }
 }
 
-exports.login =  (req,res)=>{
-   res.render("auth/login");     
+exports.login = (req, res) => {
+    res.render("auth/login");
 }
-exports.loginUser = async (req,res)=>{
+
+exports.loginUser = async (req, res) => {
     try {
-        let existuser = await User.findOne({email:req.body.email});
-        if(!existuser) return res.status(401).send("Email or password is not correct..");
-        const verified = await bcrypt.compare(req.body.password,existuser.password);
-        if(!verified) return res.status(401).send("Email or password is not correct..");
+        let existuser = await User.findOne({ email: req.body.email });
+        if (!existuser) return res.status(401).send("Email or password is not correct..");
+        const verified = await bcrypt.compare(req.body.password, existuser.password);
+        if (!verified) return res.status(401).send("Email or password is not correct..");
         // login successfully
         res.send("Login DONE");
     } catch (error) {
@@ -46,9 +60,64 @@ exports.loginUser = async (req,res)=>{
     }
 }
 
-exports.logout = (req,res)=>{
+exports.logout = (req, res) => {
     req.session.auth = null;
-    req.redirect("/auth/login");
+    res.redirect("/auth/login");
+}
+
+exports.form_forgot = (req, res) => {
+    res.render("auth/forgot");
+}
+
+exports.forgot = async (req, res) => {
+    const email = req.body.email;
+    try {
+        const user = await User.findOne({ email: email });
+        if (!user) return res.redirect("/auth/forgot-password");
+        // neu co email: 
+        const randomStr = btoa(user.email);
+        const linkReset = "http://localhost:4000/auth/reset-password?code=" + randomStr;
+        req.session.resetpassword = {
+            user: user,
+            code: randomStr
+        };
+        // send email
+        transport.sendMail({
+            from: "Demo NodeJS T2204M",
+            to: user.email,
+            cc: "",
+            subject: "Lấy lại mật khẩu tài khoản",
+            html: `<p>Click <a href="${linkReset}">here</a> to reset password.</p>`
+        });
+        res.send("VUi lòng kiểm tra email và làm theo hướng dẫn");
+    } catch (error) {
+        res.send(error);
+    }
+}
+
+exports.form_reset = (req, res) => {
+    //check code
+    const code = req.query.code;
+    const resetSession = req.session.resetpassword;
+    if (code != resetSession.code) return res.status(404).send("Error");
+    res.render("auth/reset");
+}
+
+exports.reset = async (req, res) => {
+    const new_pass = req.body.password;
+    const resetSession = req.session.resetpassword;
+    if (!resetSession || !resetSession.user) {
+        return res.status(404).send("Error");
+    }
+    const user = resetSession.user;
+    const salt = await bcrypt.genSalt(10);
+    const hashPwd = await bcrypt.hash(new_pass, salt);
+
+    await User.findByIdAndUpdate(user._id, {
+        password: hashPwd
+    });
+    req.session.resetpassword = null;
+    res.send("done");
 }
 
 // change password
